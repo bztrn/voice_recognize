12143c0d95aa90568dde7b6a9160cb16ace9f740
Alisson Zampietro <alisson@mentesnotaveis.com.br>
2016-05-09 20:09:34 -0300

    Inserindo recurso nativo dfo speaker

--- js/recognizer.js
+++ js/recognizer.js
@@ -27,14 +27,14 @@ recognizer.onresult = function(event){
 	msg.voice = voices[10]; // Note: some voices don't support altering params
 	msg.voiceURI = 'native';
 	msg.volume = 1; // 0 to 1
-	msg.text = transcript.textContent;
+	msg.text = "Você disse: " + transcript.textContent;
 	msg.lang = 'pt-BR';
 
 	msg.onend = function(e) {
 	  console.log('Finished in ' + event.elapsedTime + ' seconds.');
 	};
 
-	speechSynthesis.speak("Você disse: " + msg);
+	speechSynthesis.speak(msg);
 
 
 
